9de0ebc18e9c79a18a3adc87cc565dc8fe6f06d7
TedaLIEz <aliezted@gmail.com>
2016-06-21 15:34:21 +0800

    Thymeleaf supported

--- src/main/java/com/hustunique/SpringMvcApplication.java
+++ src/main/java/com/hustunique/SpringMvcApplication.java
@@ -2,11 +2,15 @@ package com.hustunique;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMvcApplication {
+public class SpringMvcApplication extends SpringBootServletInitializer {
+
+    private static final String DISPATCHER_SERVLET_MAPPING = "/";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/hustunique/config/WebConfig.java
@@ -0,0 +1,53 @@
+package com.hustunique.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+/**
+ * Created by JianGuo on 6/21/16.
+ * WebConfig for Adapter
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.hustunique.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    @Autowired
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(TemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    @Bean
+    public TemplateResolver templateResolver() {
+        TemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
