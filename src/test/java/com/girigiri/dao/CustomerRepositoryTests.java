fb998efb2b4ac5702fcf41a433899fc5cbd7cfe7
TedaLIEz <aliezted@gmail.com>
2016-06-26 11:10:30 +0800

    fix bug in unittest

--- src/test/java/com/girigiri/dao/CustomerRepositoryTests.java
+++ src/test/java/com/girigiri/dao/CustomerRepositoryTests.java
@@ -3,7 +3,7 @@ package com.girigiri.dao;
 import com.girigiri.SpringMvcApplication;
 import com.girigiri.dao.models.Customer;
 import com.girigiri.utils.TestUtil;
-import org.junit.Assert;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -12,20 +12,21 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-
+import static com.girigiri.utils.TestUtil.getResourceIdFromUrl;
+import static com.girigiri.utils.TestUtil.objToJson;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
@@ -65,53 +66,72 @@ public class CustomerRepositoryTests {
 
 
     @Test
-    //// FIXME: 6/26/16 Status Code always be 204, not 200!
-    public void changeCustomer() throws Exception {
-        mockMvc.perform(put("/api" + "/customers/{id}", setupId)
-                .content(TestUtil.objToJson(new Customer("my new userId", "my new mobile", "my new address", "my new contactName")))
-                .contentType(MediaType.APPLICATION_JSON))
+    public void addCustomerAndUpdate() throws Exception {
+        Customer customer = new Customer("my userId", "my mobile", "my address", "my contactName");
+        MvcResult result = mockMvc.perform(post("/api" + "/customers")
+        .content(objToJson(customer))
+        .contentType(contentType))
+                .andExpect(status().isCreated())
+                .andReturn();
+        long id = getResourceIdFromUrl(result.getResponse().getRedirectedUrl());
+        Customer c2 = new Customer("my new userId", "my new mobile", "my new address", "my new contactName");
+        mockMvc.perform(put("/api" + "/customers/{id}", id)
+                .content(objToJson(c2))
+                .contentType(contentType))
+                .andExpect(status().isNoContent())
+                .andReturn();
+        mockMvc.perform(get("/api" + "/customers/{id}", id))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userId", is("my new userId")))
-                .andExpect(jsonPath("$.contactName", is("my new contactName")))
                 .andExpect(jsonPath("$.mobile", is("my new mobile")))
-                .andExpect(jsonPath("$.address", is("my new address")));
+                .andExpect(jsonPath("$.address", is("my new address")))
+                .andExpect(jsonPath("$.contactName", is("my new contactName")));
+    }
+
+    @Test
+    public void readAllCustomers() throws Exception {
+        mockMvc.perform(get("/api" + "/customers"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$._embedded.customers", hasSize(1)));
+    }
+
+    @Test
+    public void readOneCustomer() throws Exception {
+        mockMvc.perform(get("/api" + "/customers/{id}", setupId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.userId", is("420104199601021617")))
+                .andExpect(jsonPath("$.type", is(1)))
+                .andExpect(jsonPath("$.contactName", is("my contactName")))
+                .andExpect(jsonPath("$.mobile", is("13018060139")));
+
+    }
+
+    @Test
+    public void addNewCustomer() throws Exception {
+        mockMvc.perform(post("/api" + "/customers")
+                .content(objToJson(new Customer("my userId", "my mobile", "my other address", "my other contactName")))
+                .contentType(contentType))
+                .andExpect(status().isCreated());
     }
 
 
-//    @Test
-//    public void readAllCustomers() throws Exception {
-//        mockMvc.perform(get("/api" + "/customers"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(contentType))
-//                .andExpect(jsonPath("$._embedded.customers", hasSize(1)));
-//    }
-//
-//    @Test
-//    public void readOneCustomer() throws Exception {
-//        mockMvc.perform(get("/api" + "/customers/{id}", setupId))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(contentType))
-//                .andExpect(jsonPath("$.userId", is("420104199601021617")))
-//                .andExpect(jsonPath("$.type", is(1)))
-//                .andExpect(jsonPath("$.contactName", is("my contactName")))
-//                .andExpect(jsonPath("$.mobile", is("13018060139")));
-//
-//    }
-//
-//    @Test
-//    public void addNewCustomer() throws Exception {
-//        mockMvc.perform(post("/api" + "/customers")
-//                .content(TestUtil.objToJson(new Customer("my userId", "my mobile", "my other address", "my other contactName")))
-//                .contentType(contentType))
-//                .andExpect(status().isCreated());
-//    }
+    @Test
+    public void deleteCustomer() throws Exception {
+        mockMvc.perform(delete("/api" + "/customers" + "/" + setupId))
+                .andExpect(status().isNoContent());
+    }
 
 
-//    @Test
-//    public void deleteCustomer() throws Exception {
-//        mockMvc.perform(delete("/api" + "/customers" + "/" + setupId))
-//                .andExpect(status().isNoContent());
-//    }
+    @After
+    public void onDestroy() {
+        customerRepository.deleteAll();
+    }
+
+
+
+
 
 
 
--- src/test/java/com/girigiri/utils/TestUtil.java
+++ src/test/java/com/girigiri/utils/TestUtil.java
@@ -4,9 +4,15 @@ import com.google.gson.Gson;
 
 /**
  * Created by JianGuo on 6/25/16.
+ * Utils for Unit test
  */
 public class TestUtil {
     public static String objToJson(Object object) {
         return new Gson().toJson(object);
+    }
+
+    public static long getResourceIdFromUrl(String locationUrl) {
+        String[] parts = locationUrl.split("/");
+        return Long.valueOf(parts[parts.length - 1]);
     }
 }
